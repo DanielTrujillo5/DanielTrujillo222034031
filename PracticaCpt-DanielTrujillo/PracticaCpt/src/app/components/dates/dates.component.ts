2a1420441b0a4a2b0aaf39040849859ac329cc4d
DanielTrujillo5 <danieltigo555@gmail.com>
2024-11-01 22:20:23 -0500

    feat: fin

--- PracticaCpt-DanielTrujillo/PracticaCpt/src/app/components/dates/dates.component.ts
+++ PracticaCpt-DanielTrujillo/PracticaCpt/src/app/components/dates/dates.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { FormComponent } from "../form/form.component";
+import { FormComponent } from '../form/form.component'; 
 import { EditorModule } from 'primeng/editor';
+import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 
 @Component({
   selector: 'app-dates',
   standalone: true,
-  imports: [FormComponent, EditorModule],
+  imports: [FormComponent, EditorModule,ReactiveFormsModule,CommonModule,FormsModule,CalendarModule,InputTextareaModule, FloatLabelModule],
   templateUrl: './dates.component.html',
   styleUrl: './dates.component.css'
 })
 export class DatesComponent {
+  text: string = ''; 
+  otherForm: FormGroup;
+
+  cities: any[] = [{label: 'New York', value: 'NY'}, {label: 'London', value: 'LDN'}, {label: 'Paris', value: 'PRS'}];
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.otherForm = this.fb.group({
+      name: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      city: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.otherForm.valid) {
+      console.log(this.otherForm.value);
+      this.router.navigate(['/login']); 
+    } else {
+      console.log('Formulario no valido');
+    }
+  }
+
 
 }
+
